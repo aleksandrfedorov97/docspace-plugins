890999c0318a7d0426deed95e51723f86e9a9645
sshakndr <aleksandr.kondratyev@onlyoffice.com>
2025-06-17 10:48:31 +0700

    fix(draw.io): check for Download permission to open the file

--- draw.io/src/Drawio.ts
+++ draw.io/src/Drawio.ts
@@ -306,6 +306,15 @@ class DrawIo {
       //@ts-ignore
       const { title, security } = file;
 
+      if (!security?.Download) {
+        return {
+          actions: [Actions.showToast],
+          toastProps: [
+            { type: ToastType.error, title: "You don't have permission to view this file" } as IToast,
+          ],
+        };
+      }
+
       const showSaveButton =
         security?.Edit || //@ts-ignore
         file.access === 0 || //@ts-ignore
